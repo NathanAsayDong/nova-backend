f0e09f8c36044e49919d4ca3a69f46697f3dceb5
Nathan Dong <nathanasaydong@gmail.com>
2026-09-03 16:31:15 -0600

    origins

--- main.py
+++ main.py
@@ -43,6 +43,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://10.1.10.199:5173",
+        "http://10.1.10.199:8000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
